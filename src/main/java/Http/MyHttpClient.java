8bf279962dfa36fbd7af9cabfa068e17f31afa0a
yongshengL <1285087082@qq.com>
2018-09-11 16:51:56 +0800

    把get请求单独写出来

--- src/main/java/Http/MyHttpClient.java
+++ src/main/java/Http/MyHttpClient.java
@@ -2,13 +2,21 @@ package Http;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MyHttpClient {
 
@@ -29,6 +37,45 @@ public class MyHttpClient {
                 result = EntityUtils.toString(entity);
             }
             System.out.println(statusLine.getStatusCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /*
+        负责处理post请求
+     */
+    public String httpPost(String url,Map<String,String> param){
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+        String result=null;                         //respon的数据
+        List<NameValuePair> nvp = new ArrayList<NameValuePair>();
+        if(param!=null){
+            for(Map.Entry<String,String> entry: param.entrySet()){
+                nvp.add(new BasicNameValuePair(entry.getKey(),entry.getValue()));
+            }
+        }
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nvp, "utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println("请求地址："+url);
+        System.out.println("请求参数："+nvp.toString());
+        httpPost.setHeader("Content-type", "application/x-www-form-urlencoded");
+        httpPost.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        HttpResponse response=null;
+        try {
+            response = httpClient.execute(httpPost);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpEntity entity = response.getEntity();
+        try {
+             result = EntityUtils.toString(entity);
+            System.out.println(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
