ccef70653212b40da5ec14950bcd728220131546
yongshengL <1285087082@qq.com>
2018-09-11 16:38:00 +0800

    第一次提交

--- src/main/java/Http/MyHttpClient.java
+++ src/main/java/Http/MyHttpClient.java
@@ -1,5 +1,14 @@
 package Http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.util.Map;
 
 public class MyHttpClient {
@@ -7,7 +16,24 @@ public class MyHttpClient {
     public String HttpGet(String url){
 
 
-        return null;
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        String result=null;
+        HttpResponse httpResponse = null;
+        HttpEntity entity  = null;
+        try {
+            httpResponse = httpClient.execute(httpGet);
+            StatusLine statusLine  = httpResponse.getStatusLine();
+            if(statusLine.getStatusCode()==200){
+                entity = httpResponse.getEntity();
+                result = EntityUtils.toString(entity);
+//                TOKEN = httpResponse.getFirstHeader().g
+            }
+            System.out.println(statusLine.getStatusCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
 
     }
 }
